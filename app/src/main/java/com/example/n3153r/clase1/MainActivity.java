d5c76169d4dbb044c8c97a3eafdef5c870f1cd9b
N3153R <neiss.rodriguez@gmail.com>
2016-09-10 17:41:11 -0500

    oka

--- app/src/main/java/com/example/n3153r/clase1/MainActivity.java
+++ app/src/main/java/com/example/n3153r/clase1/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.n3153r.clase1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button boton3;
-    String mensaje1;
+    String mensaje1,mensajeArray;
+
+    TextView textView;
+    String[] provincias;
+    int[] distancias;
 
 
     @Override
@@ -23,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
         boton3.setTextSize(dimension);
         int color = getResources().getColor(R.color.verde);
         boton3.setTextColor(color);
+
+        textView = (TextView) findViewById(R.id.txtArray);
+        provincias = getResources().getStringArray(R.array.provincias);
+        distancias = getResources().getIntArray(R.array.distancias);
+        mensajeArray ="";
+        for (int i=0; i<provincias.length; i++)
+            mensajeArray +=provincias[i] + " - " + distancias[i] + " \n";
+
+        textView.setText(mensajeArray);
     }
 }

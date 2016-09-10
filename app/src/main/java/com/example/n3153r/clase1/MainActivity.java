3e55d0700486253ad43392a4012ba8c8287ea748
N3153R <neiss.rodriguez@gmail.com>
2016-09-10 18:23:42 -0500

    oka

--- app/src/main/java/com/example/n3153r/clase1/MainActivity.java
+++ app/src/main/java/com/example/n3153r/clase1/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.n3153r.clase1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button boton3;
-    String mensaje1,mensajeArray;
+    Button boton3,btnNuevo;
+    String mensaje,mensajeArray;
 
     TextView textView;
     String[] provincias;
     int[] distancias;
+
+
 
 
     @Override
@@ -21,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         boton3=(Button) findViewById(R.id.button3);
-        mensaje1=getResources().getString(R.string.mensaje1);
-        boton3.setText(mensaje1);
+        mensaje=getResources().getString(R.string.mensaje3);
+        boton3.setText(mensaje);
 
         float dimension = getResources().getDimension(R.dimen.tipo_grande);
         boton3.setTextSize(dimension);
-        int color = getResources().getColor(R.color.verde);
+        int color = getResources().getColor(R.color.amarillo);
         boton3.setTextColor(color);
 
         textView = (TextView) findViewById(R.id.txtArray);
@@ -37,5 +42,26 @@ public class MainActivity extends AppCompatActivity {
             mensajeArray +=provincias[i] + " - " + distancias[i] + " \n";
 
         textView.setText(mensajeArray);
+
+//        nueva ventana
+
+        btnNuevo =(Button) findViewById(R.id.btnNueva);
+        btnNuevo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case  R.id.btnNueva:
+                Intent intent = new Intent(this,Main2Activity.class);
+//
+                startActivity(intent);
+
+                break;
+            default:
+                break;
+        }
     }
 }

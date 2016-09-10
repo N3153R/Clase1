d1160713e91dd5a901547f42f4d07868c008a2ef
N3153R <neiss.rodriguez@gmail.com>
2016-09-10 17:20:00 -0500

    oka

--- app/src/main/java/com/example/n3153r/clase1/MainActivity.java
+++ app/src/main/java/com/example/n3153r/clase1/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.n3153r.clase1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton3;
+    String mensaje1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton3=(Button) findViewById(R.id.button3);
+        mensaje1=getResources().getString(R.string.mensaje1);
+        boton3.setText(mensaje1);
+
+        float dimension = getResources().getDimension(R.dimen.tipo_grande);
+        boton3.setTextSize(dimension);
+        int color = getResources().getColor(R.color.verde);
+        boton3.setTextColor(color);
     }
 }
-//holaaaaaaaa
